b2b88f9703f598466096dfb26cce13343abaa7e4
Chen Haiqin <632903739@qq.com>
2019-05-28 22:07:36 +0800

    Update DaggerBaseActivity.java

--- app/src/main/java/per/chq/dagger2simpleforandroid/dagger/DaggerBaseActivity.java
+++ app/src/main/java/per/chq/dagger2simpleforandroid/dagger/DaggerBaseActivity.java
@@ -8,6 +8,7 @@ import dagger.android.AndroidInjection;
 
 //Step 5:在activity里配置如下，或创建Activity基类，将需要使用Dagger的activity继承该类
 //fragment 及 android的其它三个组件类似配置
+//fragment 在onAttach()里AndroidSupportInjection.inject(this);
 public class DaggerBaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
